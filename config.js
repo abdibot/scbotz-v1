1d6a73f24098b4e436bc760236b6f09fc36029ee
VANZZ OFC <96647853+abdibot@users.noreply.github.com>
2022-04-03 14:51:31 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -13,9 +13,9 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.linkGC = ['https://chat.whatsapp.com/EVCGfzxLWfp81n0WhmVklH', 'https://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
-global.owner = ['60189830350', '60149431385', '60189830350'] // Masukan nomot kalian
-global.mods = ['60189830350', '60149431385'] // Moderator
+global.linkGC = ['https://chat.whatsapp.com/KazGpGgVJv1K9TkpzrZoFL', 'https://chat.whatsapp.com/GPjpcjs9AvvH96ePd2tsNM', 'https://chat.whatsapp.com/GSeZ2CA4ubaENzDt0b5ChK'] // ganti jadi group lu
+global.owner = ['6285824629954', '6285824629954', '6285824629954'] // Masukan nomot kalian
+global.mods = ['6285824629954', '6285824629954'] // Moderator
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -54,8 +54,8 @@ global.APIKeys = { // APIKey Here
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'LynXzy🥀'
-  var sticker_author = '©LynXzy🥀    ?'
+  var sticker_name = 'HELLO BOT'
+  var sticker_author = '©HELLO BOT'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -79,7 +79,7 @@ global.nsfw = '“Katakanlah kepada orang laki-laki yang beriman: Hendaklah mere
 global.eror = '_*Error*_'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23101820&text='
-global.watermark = '©LynXzy🥀    ?' //change the watermark (beberapa harus manual)
+global.watermark = '©HELLO BOT' //change the watermark (beberapa harus manual)
 
 // Ubah saja
 global.image = 'https://telegra.ph/file/a60af91a1c5b4e12967fd.jpg'//thumbnail
